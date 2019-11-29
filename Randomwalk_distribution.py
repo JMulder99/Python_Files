26c483b230c3c12281045ca76f74cf5d34866406
Jelmer Mulder <jmulder14.jm@gmail.com>
2019-11-29 23:54:46 +0100

    first commit

---
+++ Randomwalk_distribution.py
@@ -0,0 +1,28 @@
+# Rondom-walk distribution
+# Jelmer Mulder
+
+import random
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+N = 100000
+def create_empty_list(N):
+    xpos = []
+    ypos = []
+    for i in range(N):
+        xpos.append(0)
+        ypos.append(0)
+    return xpos, ypos
+
+xpos, ypos = create_empty_list(N)
+steps = 0
+while steps < 100:
+    for i in range(N):
+        xpos[i-1] += math.cos(random.random()*2*math.pi)
+        ypos[i-1] += math.sin(random.random()*2*math.pi)
+    steps += 1
+#fig1 = plt.figure()
+plt.plot(xpos, ypos, 'bo', markersize=1)
+plt.xlim(-40, 40)
+plt.ylim(-40, 40)
+plt.show()
