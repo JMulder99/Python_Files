9a1609662cb7a5d259c1cbcb7cd561c1c592a490
Jelmer Mulder <jmulder14.jm@gmail.com>
2019-11-30 00:11:21 +0100

    add random walk function basic

--- Randomwalk_distribution.py
+++ Randomwalk_distribution.py
@@ -1,6 +1,7 @@
 # Rondom-walk distribution
 # Jelmer Mulder
-
+import time
+start = time.time()
 import random
 import math
 import matplotlib.pyplot as plt
@@ -13,16 +14,21 @@ def create_empty_list(N):
         xpos.append(0)
         ypos.append(0)
     return xpos, ypos
-
 xpos, ypos = create_empty_list(N)
+
 steps = 0
-while steps < 100:
-    for i in range(N):
-        xpos[i-1] += math.cos(random.random()*2*math.pi)
-        ypos[i-1] += math.sin(random.random()*2*math.pi)
-    steps += 1
+def points_gen(steps, N):
+    while steps < 100:
+        for i in range(N):
+            xpos[i-1] += math.cos(random.random()*2*math.pi)
+            ypos[i-1] += math.sin(random.random()*2*math.pi)
+        steps += 1
+    return xpos, ypos
+xpos, ypos = points_gen(steps, N)
+print(time.time() - start)
 #fig1 = plt.figure()
 plt.plot(xpos, ypos, 'bo', markersize=1)
 plt.xlim(-40, 40)
 plt.ylim(-40, 40)
 plt.show()
+
